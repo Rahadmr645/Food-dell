be1646f973460ffeb711e5485af239467a00f8ec
Rahad <rahadmr645@gmail.com>
2025-01-13 00:56:15 +0300

    four

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Hero from '../Hero/Hero'
 import Menu from '../menu/Menu'
 import FoodList from '../FoodList/FoodList'
-import DownloadApp from '../DownLoadApp/DownloadApp'
+// import DownloadApp from '../DownLoadApp/DownloadApp'
 import './Home.css'
 const Home = () => {
   const [category, SetCategory] = useState("All");
@@ -15,7 +15,7 @@ const Home = () => {
       <Hero />
       <Menu category={category} SetCategory={SetCategory} />
       <FoodList category={category} />
-       <DownloadApp/>
+       {/* <DownloadApp/> */}
     </div>
   )
 }
