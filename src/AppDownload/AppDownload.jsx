6fff19fb9fdb59fb8eef19a74ae8bceb3eca8a95
Rahad <rahadmr645@gmail.com>
2025-01-13 22:42:09 +0300

    six

--- src/AppDownload/AppDownload.jsx
+++ src/AppDownload/AppDownload.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-import './AppDownload.css'
+import { assets } from '../assets/assets'
+import './AppDownload.css';
 const AppDownload = () => {
   return (
-    <div>AppDownload</div>
+    <div className='container download-container'>
+      <div className="text-h1">
+         <h1>For better Expreince Dowenload </h1> 
+        <h1>Tomato App</h1>
+      </div>
+        <div className="app-button">
+          <img src={assets.app_store} alt="app_store" />
+          <img src={assets.play_store} alt="playsoter" />
+        </div>
+    </div>
   )
 }
 
-export default AppDownload
+export default AppDownload;
