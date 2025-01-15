4ce3a482c6b7ccc5ecc628107699ce31a45a8825
Rahad <rahadmr645@gmail.com>
2025-01-15 04:24:30 +0300

    improve cart and login

--- src/carts/Carts.jsx
+++ src/carts/Carts.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from '../context/StoreConext';
 
 const Carts = () => {
 
-  const { cartItems, addToCart, removeFromCart, food_list } = useContext(StoreContext);
+  const { cartItems, removeFromCart, food_list } = useContext(StoreContext);
 
   return (
     <div className='container cart-container'>
@@ -20,27 +20,52 @@ const Carts = () => {
         </div>
         <hr className='title-hr' />
       </div>
-
-      {food_list.map((item, index) => {
-        if (cartItems[item._id] > 0) {
-          return (
-            <>
-              <div className='cart-item-title cart-foodlist'>
-                <img src={item.image} alt="" />
-                <p>{item.name}</p>
-                <p>{item.price}</p>
-                <p>{cartItems[item._id]}</p>
-                <p>{cartItems[item._id] * item.price}</p>
-                <p>x</p>
-              </div>
-              <hr  className='hr'/>
-            </>
-          )
-        }
-
-      })}
-
+      <div className=" cart-info"  >
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <>
+                <div className="carts-item-title ">
+                  <img src={item.image} alt='' />
+                  <p>{item.name}</p>
+                  <p >{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{cartItems[item._id] * item.price}</p>
+                  <p onClick={() => removeFromCart(item._id)}>x</p>
+                </div>
+                <hr className='hr-row' />
+              </>
+            )
+          }
+        })}
+      </div>
+      <div className='carts-bottom'>
+        <div className='carts-totals'>
+          <h1>Carts Total</h1>
+          <div className='carts-total-para'>
+            <h4>Subtotal</h4>
+            <p>0</p>
+          </div>
+          <hr className='totals-hr' />
+          <div className='carts-total-para'>
+            <h4>Delivery fee</h4>
+            <p>2</p>
+          </div>
+          <hr className='totals-hr' />
+          <div className='carts-total-para'>
+            <h4>Total</h4>
+            <p>0</p>
+          </div>
+          <button>PROCEED TO CHACKOUT</button>
+        </div>
+        <div className="promocode">
+          <h4>If you have any promocode, Enter it here</h4>
+          <input type='text' placeholder='promo code' />
+          <button>Submit</button>
+        </div>
+      </div>
     </div>
+
   )
 }
 
